861da2c74ef07461735162246b50c05534f81e6a
ssamal6236 <87814454+ssamal6236@users.noreply.github.com>
2022-06-20 22:01:12 -0400

    Product route created

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true, useUnifiedTopology:true}).then((data)=>{
-        //useCreateIndex:true is not support in above line
-        console.log(`Mongodb connected with server: ${data.connection.host}`);
-    }).catch((err)=>{
-        console.log(err);
+const connectDatabase = () => {
+    mongoose
+    .connect(process.env.DB_URI, {
+        useNewUrlParser:true, 
+        useUnifiedTopology:true
+        //useCreateIndex:true is not support
     })
+    .then((data) => {        
+        console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    //commenting below step due to Unhandled Promise Rejecting coded in server.js
+    // .catch((err)=>{
+    //     console.log(err);
+    // });
 }
 
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,81 @@
-exports.getAllProducts = (req,res)=>{
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
 
-    res.status(200).json({message:"Route is working fine"})
-}
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+//Create Product - Admin
+exports.createProduct = catchAsyncErrors(async(req,res,next)=>{
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product,
+    });
+});
+
+
+
+//Get all Products
+exports.getAllProducts = catchAsyncErrors(async(req,res)=>{
+
+    const products = await Product.find();
+    res.status(200).json({
+        success:true,
+        products,
+    });
+});
+
+//Get Product details
+exports.getProductDetails = catchAsyncErrors(async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    });
+});
+
+//Update a product by id - Admin
+exports.updateProduct = catchAsyncErrors(async(req,res,next)=>{
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    });
+});
+
+
+//Delete a Product - Admin
+exports.deleteProduct = catchAsyncErrors(async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"Product Successfully Deleted"
+    })
+});
